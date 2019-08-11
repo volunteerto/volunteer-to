d7ab148cf025e3ab9595363ef403297876a4d92e
Ethan Peterson <ethan@Ethans-MacBook-Pro.local>
2019-08-10 21:28:51 -0400

    added hello world test endpoint to API

--- internal/api/router.go
+++ internal/api/router.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi"
@@ -8,6 +9,10 @@ import (
 
 func health(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(http.StatusOK)
+}
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 }
 
 // New returns an 'http.Handler' that serves the ship-it API.
@@ -19,6 +24,7 @@ func New() http.Handler {
 
 	r.Route("/api", func(r chi.Router) {
 		// List Endpoints here
+		r.Get("/hello-world", helloWorld)
 	})
 
 	return r
